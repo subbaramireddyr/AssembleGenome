29fd5ca60edc8bb477f441502c23690227d0e65f
Subbaramireddy Remala <135566671+subbaramireddyr@users.noreply.github.com>
2024-10-10 12:45:09 -0400

    getNGS.sh

--- getNGS.sh
+++ getNGS.sh
@@ -2,6 +2,4 @@
 # getNGS.sh
 
 # Retrieve the specified NGS reads.
-# split 3 used for paired-end reads 1 file for forward reads and 2 file for reverse reads 
-#3rd file for unpaired reads
 fasterq-dump --split-3 SRRNumber
