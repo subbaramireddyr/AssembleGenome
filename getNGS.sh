d9bcdf56995dce5dbf160c85eac64b41e32c7580
Subbaramireddy Remala <135566671+subbaramireddyr@users.noreply.github.com>
2024-10-10 12:34:57 -0400

    getNGS.sh

--- getNGS.sh
+++ getNGS.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # getNGS.sh
 
-# Retrieve the Rhodobacter spheroides NGS reads.
-fasterq-dump --split-3 SRR522244
+# Retrieve the specified NGS reads.
+# split 3 used for paired-end reads 1 file for forward reads and 2 file for reverse reads 
+#3rd file for unpaired reads
+fasterq-dump --split-3 SRRNumber
